c5577a296d765095521a4cf0174f9c21344ba5f2
Max Surnin <madmax8930@gmail.com>
2022-03-02 16:51:54 +0100

    port update

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+var port = process.env.PORT || 80
+
 app.use(express.static(dirname + '/dist/live-plante'));
 
 app.get('/*', function(req,res) {
 res.sendFile(path.join(dirname+
 '/dist/live-plante/index.html'));});
 
-app.listen(process.env.PORT || 8080);
+app.listen(port);
