8fcbfb998f22ce26aad29be8d591d69deddf2608
Max Surnin <madmax8930@gmail.com>
2022-03-02 12:57:30 +0100

    check

--- server.js
+++ server.js
@@ -1,10 +1,3 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('products.json');
-const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 80;
-
-server.use(middlewares);
-server.use(router);
 
 server.listen(port);
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrlApi: 'https://la-belle-plante-test.herokuapp.com'
 };
