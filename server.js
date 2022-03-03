05137a9e87d87c14d98109b08a4a631344eac5c9
Max Surnin <madmax8930@gmail.com>
2022-03-03 16:12:32 +0100

    done

--- server.js
+++ server.js
@@ -5,10 +5,9 @@ const app = express();
 
 var port = process.env.PORT || 80
 
-app.use(express.static(dirname + '/dist/plantes'));
+app.use(express.static(__dirname + '/dist/plantes'));
 
 app.get('/*', function(req,res) {
-res.sendFile(path.join(dirname+
-'/dist/plantes/index.html'));});
+res.sendFile(path.join(__dirname + '/dist/plantes/index.html'));});
 
 app.listen(port);
